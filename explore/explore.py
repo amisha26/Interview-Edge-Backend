840626faefd12e6536127bb78da66942d28250a1
amisha26 <amisha8299@gmail.com>
2023-09-13 22:30:00 -0400

    selected topic done

--- explore/explore.py
+++ explore/explore.py
@@ -143,7 +143,6 @@ def explore_route(connection):
                     title_name = topicMappping[urltitle]
                 formattedData = {"title": title_name, "urlTitle": urltitle, "total": total, "solved": solved}
                 topic_data.append(formattedData)
-            print(topic_data)
             queryRes = {"data":"name","onGoingTopic":False }
             finalData = {"data":topic_data,"onGoingTopic":queryRes}
             #finalData = {"data":dummyData,"onGoingTopic":queryRes} 
@@ -159,7 +158,28 @@ def explore_route(connection):
     def getSelectedTopic():
         try:
             id, topic = request.args.get("id"), request.args.get("topic")
-            return jsonify({"data": selectedTopicData, "error": False}), 200
+            #query = f"SELECT q.question_id, q.question_url, q.question_name, q.level, q.platform AS platform, COUNT(uq.question_id) AS user_question_count FROM userQuestions uq RIGHT JOIN questions q ON q.question_id = uq.question_id AND uq.user_id = '{id}' WHERE (uq.user_id = '{id}' OR uq.user_id IS NULL) AND uq.topic_name = '{topic}'"
+            query = f"SELECT q.question_url, q.question_id, q.topic_name, q.question_name, q.level, q.platform, CASE WHEN uq.user_id IS NOT NULL THEN TRUE ELSE FALSE END AS completed FROM questions q LEFT JOIN userQuestions uq ON q.question_id = uq.question_id AND uq.user_id = '{id}' WHERE (uq.user_id = '{id}' OR uq.user_id IS NULL) AND q.topic_name ='{topic}' order by q.question_name"
+            queryRes = dbObj.selectQuery(query, False)
+            #print(queryRes)
+            easyArr, mediumArr, hardArr = [], [], [] 
+            for data in queryRes:
+                question_url, question_id, topic_name, question_name, level, platform, completed = data[0], data[1], data[2], data[3], data[4], data[5], data[6]
+                if level == "easy":
+                    formattedData = {"completed": completed, "id": question_id, "name": question_name, "platform": platform, "url": question_url}
+                    easyArr.append(formattedData)
+                elif level == "medium":
+                    formattedData = {"completed": completed, "id": question_id, "name": question_name, "platform": platform, "url": question_url}
+                    mediumArr.append(formattedData)
+                elif level == "hard":
+                    formattedData = {"completed": completed, "id": question_id, "name": question_name, "platform": platform, "url": question_url}
+                    hardArr.append(formattedData)
+            easyA = {"body": easyArr, "cardTitle": "Easy", "cardType": "easy"}
+            mediumA = {"body": mediumArr, "cardTitle": "Medium", "cardType": "medium"}
+            hardA = {"body": hardArr, "cardTitle": "Hard", "cardType": "hard"}
+            selectedTopicData1 = [easyA, mediumA, hardA]
+            print("deee: ", selectedTopicData1)
+            return jsonify({"data": selectedTopicData1, "error": False}), 200
         
         except Exception as e:
             print(e)
