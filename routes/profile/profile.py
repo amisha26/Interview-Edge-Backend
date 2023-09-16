60cf8f6482fef951e24c030e0c8e4cd350284ae8
amisha26 <amisha8299@gmail.com>
2023-09-16 16:59:22 -0400

    code refactoring done for profile

--- routes/profile/profile.py
+++ routes/profile/profile.py
@@ -17,26 +17,9 @@ def profile_route(connection):
     def user_status():
         try:
             id = request.args.get('id')
-            query = f"SELECT q.level, COUNT(*) AS level_count, COUNT(DISTINCT uq.question_id) FROM questions q LEFT JOIN userquestions uq ON q.question_id = uq.question_id WHERE uq.user_id = '{id}' or uq.user_id IS NULL GROUP BY q.level"
-            queryRes = dbObj.selectQuery(query, False)
-            query1 = f"SELECT count(q.question_id), count(uq.userquestions_id) FROM questions q LEFT JOIN userquestions uq ON q.question_id = uq.question_id"
-            queryRes1 = dbObj.selectQuery(query1, False)
-            print(queryRes)
-            print(queryRes1)
-            total, totalSolved = queryRes1[0][0], queryRes1[0][1]
-            for data in queryRes:
-                if data[0] == "easy":
-                    easyTotal = data[1]
-                    easySolved = data[2]
-                elif data[0] == "medium":
-                    mediumTotal = data[1]
-                    mediumSolved = data[2]
-                elif data[0] == "hard":
-                    hardTotal = data[1]
-                    hardSolved = data[2]
-            user_data1 = {"easySolved": easySolved, "easyTotal": easyTotal, "hardSolved": hardSolved, "hardTotal": hardTotal, "mediumSolved": mediumSolved, "mediumTotal": mediumTotal, "total": total, "totalSolved": totalSolved}
+            user_data1 = profileObj.getUserStatus(id)
             return jsonify({"data": user_data1, "error": False}), 200
-            #return jsonify({"data": user_data1, "error": False}), 200
+            #return jsonify({"data": user_data, "error": False}), 200
 
         except Exception as e:
             print(e)
--- routes/profile/profile_db.py
+++ routes/profile/profile_db.py
@@ -11,7 +11,24 @@ class ProfileDb:
     def __init__(self, connection):
         self.dbObj = Database(connection)
 
-
+    def getUserStatus(self, id):
+        query = f"SELECT q.level, COUNT(*) AS level_count, COUNT(DISTINCT uq.question_id) FROM questions q LEFT JOIN userquestions uq ON q.question_id = uq.question_id WHERE uq.user_id = '{id}' or uq.user_id IS NULL GROUP BY q.level"
+        queryRes = self.dbObj.selectQuery(query, False)
+        query1 = f"SELECT count(q.question_id), count(uq.userquestions_id) FROM questions q LEFT JOIN userquestions uq ON q.question_id = uq.question_id"
+        queryRes1 = self.dbObj.selectQuery(query1, False)
+        total, totalSolved = queryRes1[0][0], queryRes1[0][1]
+        for data in queryRes:
+            if data[0] == "easy":
+                easyTotal = data[1]
+                easySolved = data[2]
+            elif data[0] == "medium":
+                mediumTotal = data[1]
+                mediumSolved = data[2]
+            elif data[0] == "hard":
+                hardTotal = data[1]
+                hardSolved = data[2]
+        user_data1 = {"easySolved": easySolved, "easyTotal": easyTotal, "hardSolved": hardSolved, "hardTotal": hardTotal, "mediumSolved": mediumSolved, "mediumTotal": mediumTotal, "total": total, "totalSolved": totalSolved}
+        return user_data1
         
 
     def getTableData(self, id):
