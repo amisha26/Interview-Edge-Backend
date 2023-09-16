746df275cea36563031541dd46a028ce8a962a61
amisha26 <amisha8299@gmail.com>
2023-09-16 17:17:35 -0400

    moved topic mapping to project data folder

--- routes/explore/explore_db.py
+++ routes/explore/explore_db.py
@@ -1,14 +1,9 @@
 from routes.database.database import Database
+from projData.topicData import topicMapping
 
 
 class ExploreDb:
-    topicMappping = {"twoPointers": "Two Pointers",
-                    "strings": "Strings", "arrays": "Arrays","stack":"Stack",
-                    "binarySearch":"Binary Search","linkedlist":"Linked List",
-                    "tree-1":"Tree - 1","tree-2":"Tree - 2","dp-1":"Dynamic Programming - 1",
-                    "heap":"Heap - Priority Queue","dp-2":"Dynamic Programming - 2",
-                    "slidingWindow":"Sliding Window"}
-    
+
     def __init__(self, connection):
         self.dbObj = Database(connection)
 
@@ -18,8 +13,8 @@ class ExploreDb:
         topic_data = []
         for data in title:
             urltitle, total, solved = data[0], data[1], data[2]
-            if urltitle in self.topicMappping:
-                title_name = self.topicMappping[urltitle]
+            if urltitle in topicMapping:
+                title_name = topicMapping[urltitle]
             formattedData = {"title": title_name, "urlTitle": urltitle, "total": total, "solved": solved}
             topic_data.append(formattedData)
         queryRes = {"data":"name","onGoingTopic":False }
--- routes/profile/profile_db.py
+++ routes/profile/profile_db.py
@@ -1,12 +1,7 @@
 from routes.database.database import Database
+from projData.topicData import topicMapping
 
 class ProfileDb:
-    topicMappping = {"twoPointers": "Two Pointers",
-                    "strings": "Strings", "arrays": "Arrays","stack":"Stack",
-                    "binarySearch":"Binary Search","linkedlist":"Linked List",
-                    "tree-1":"Tree - 1","tree-2":"Tree - 2","dp-1":"Dynamic Programming - 1",
-                    "heap":"Heap - Priority Queue","dp-2":"Dynamic Programming - 2",
-                    "slidingWindow":"Sliding Window"}
     
     def __init__(self, connection):
         self.dbObj = Database(connection)
@@ -37,8 +32,8 @@ class ProfileDb:
         rows = []
         for data in queryRes:
             date, done, level, platform, question, url = data[5], data[6], data[3], data[4], data[1], data[2]
-            if data[0] in self.topicMappping:
-                topic = self.topicMappping[data[0]]
+            if data[0] in topicMapping:
+                topic = topicMapping[data[0]]
             if done == 1:
                 done = "Yes"
             else:
